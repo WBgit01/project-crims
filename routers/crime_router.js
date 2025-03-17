c4360e9ca316f9f100c2dcc240adf53932b8bae4
Marc Malacas <malacasmarc16@gmail.com>
2025-03-17 16:31:56 +0800

    Updated

--- routers/crime_router.js
+++ routers/crime_router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {getCrimes, getCrime, reportCrime, updateCrime} = require('../controllers/crime_controller.js');
 
-router.get('/crime', getCrimes);
-router.get('/crime:id', getCrime);
-router.post('/crime', reportCrime);
-router.put('/crime:id', updateCrime);
+router.get('/', getCrimes);
+router.get('/:crime_id', getCrime);
+router.post('/', reportCrime);
+router.put('/:crime_id', updateCrime);
 
 module.exports = router;
--- routers/resource_router.js
+++ routers/resource_router.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {getResources, getResource, createResource, updateResource, deleteResource} = require('../controllers/resources_controller.js');
 
-router.get('/resources', getResources);
-router.get('/resouces/:id', getResource);
-router.post('/resources', createResource);
-router.put('/resource/:id', updateResource);
-router.delete('/resource/:id', deleteResource);
+router.get('/', getResources);
+router.get('/:id', getResource);
+router.post('/', createResource);
+router.put('/:id', updateResource);
+router.delete('/:id', deleteResource);
 
 module.exports = router;
